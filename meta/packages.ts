a9661f9d3a3360ce3268a375ea9838552effbd08
zekun.jin <zekun.jin@connext.com.cn>
2022-09-07 16:52:56 +0800

    chore: remove unused vars

--- meta/packages.ts
+++ meta/packages.ts
@@ -19,12 +19,4 @@ interface PackageManifest {
   copy?: string[]
 }
 
-export const packages: PackageManifest[] = [
-  {
-    name: 'core',
-    display: 'yor',
-    build: true,
-    cjs: true,
-    mjs: true
-  }
-]
+export const packages: PackageManifest[] = [{ name: 'core', display: 'yor' }]
