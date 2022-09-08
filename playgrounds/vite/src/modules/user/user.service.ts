485fa189b97d93667ea3b23c58dc7bc29fbffd00
zekun.jin <zekun.jin@connext.com.cn>
2022-09-08 11:09:04 +0800

    chore: rm unused type

--- playgrounds/vite/src/modules/user/user.service.ts
+++ playgrounds/vite/src/modules/user/user.service.ts
@@ -1,12 +1,11 @@
 import { defineProvider } from '@yor/core'
 import { LoggingInterceptor } from '../../common/logging.interceptor'
-import { SignInDto } from './dto/sign-in.dto'
 import { UserRepository } from './interfaces/user-repository.interface'
 import { UserService } from './interfaces/user-service.interface'
 import { UserRepositoryImpl } from './user.repository'
 
 const UserServiceImpl = defineProvider<UserService>((userRepository: UserRepository) => ({
-  signIn(data: SignInDto) {
+  signIn(data) {
     return userRepository.signIn(data)
   },
 
