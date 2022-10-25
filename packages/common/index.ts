167d34bc893ba891f95ff28bb47f97c7380839ed
zekun.jin <zekun.jin@foxmail.com>
2022-10-25 15:16:14 +0800

    build: rm re-export modules

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,4 +1,2 @@
-export * from './src/providers/local-storage.provider'
-export * from './src/providers/session-storage.provider'
 export * from './src/providers/web-storage.provider'
 export * from './src/providers/pagination.provider'
