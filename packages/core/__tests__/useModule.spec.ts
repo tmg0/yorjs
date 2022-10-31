cebaefdf53c6d79f9cffe115cc28e47cb2f14e27
zekun.jin <zekun.jin@foxmail.com>
2022-10-31 10:16:41 +0800

    feat: export provider in modules

--- packages/core/__tests__/useModule.spec.ts
+++ packages/core/__tests__/useModule.spec.ts
@@ -1,4 +1,4 @@
-import { defineController, defineInterface, defineModule, defineProvider, useModule, useProvider } from '../index'
+import { defineController, defineInterface, defineModule, defineProvider, useModule } from '../index'
 
 describe('use module', () => {
   const IP1 = defineInterface<{ do1: () => string }>()
@@ -10,30 +10,29 @@ describe('use module', () => {
 
   const p2 = defineProvider().implements(IP2).inject(IP1).setup(p1 => ({
     do2: p1.do1
-  })) 
+  }))
+
+  const IC = defineInterface<{ do: (v: string) => string }>()
+
+  const c = defineController().implements(IC).inject(IP1, IP2).setup((p1, p2) => {
+    return {
+      do(v) {
+        if (typeof v === 'string')
+          return p1.do1()
+        return p2.do2()
+      }
+    }
+  })
+
+  const m = defineModule({ controller: c, providers: [p1, p2], exports: [p2] })
 
   it('should have controller method in module', () => {
-    const IC = defineInterface<{ do: (v: string) => string }>()
-
-    const c = defineController().implements(IC).inject(IP1, IP2).setup((p1, p2) => {
-      return {
-        do(v) {
-          if (typeof v === 'string')
-            return p1.do1()
-          return p2.do2()
-        }
-      }
-    })
-
-    const m = defineModule({ controller: c, providers: [p1, p2] })
-
     const { do: mDo } = useModule(m)
-
     expect(mDo('')).toBe('STR')
   })
 
   it('should inject impls when use provider', () => {
-    const { do2 } = useProvider(p2)
+    const { do2 } = m.useExport(p2)
     expect(do2()).toBe('STR')
   })
 })
--- packages/core/src/defineModule/index.ts
+++ packages/core/src/defineModule/index.ts
@@ -1,3 +1,4 @@
+import { useProvider } from '../useProvider'
 import type { Controller } from '../defineController'
 import type { Provider } from '../defineProvider'
 
@@ -15,25 +16,34 @@ const injectImpls = (ctx: Provider<any> | Controller<any>, _providers: Provider<
   return deps
 }
 
-
 export interface ModuleOptions<T> {
   controller: Controller<T>
   providers?: Provider<unknown>[]
+  exports?: Provider<unknown>[]
 }
 
 export class Module<T> {
   public controller: Controller<T>
   public providers: Provider<unknown>[]
+  public exports: Provider<unknown>[]
 
   constructor(options: ModuleOptions<any>) {
     this.controller = options.controller
     this.providers = options.providers || []
+    this.exports = options.exports || this.providers || []
 
     this.controller.dependencies(...injectImpls(this.controller), this.providers)
 
     this.providers.forEach((provider) => {
       provider.dependencies(...injectImpls(provider))
     })
+  }
+
+  useExport<T>(provider: Provider<T>): T {
+    if (this.exports.some(({ token }) => token === provider.token))
+      return useProvider(provider)
+    else
+      throw new Error('should export current provider in this module')
   }
 }
 
