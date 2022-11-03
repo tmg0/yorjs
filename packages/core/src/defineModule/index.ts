b8563f5b522a2109b2a35bc5acef45e40a3cb9e4
zekun.jin <zekun.jin@foxmail.com>
2022-11-03 11:31:31 +0800

    feat: add inject impls error msg

--- packages/core/src/defineModule/index.ts
+++ packages/core/src/defineModule/index.ts
@@ -2,16 +2,30 @@ import { useProvider } from '../useProvider'
 import type { Controller } from '../defineController'
 import type { Provider } from '../defineProvider'
 
-const injectImpls = (ctx: Provider<any> | Controller<any>, _providers: Provider<unknown>[] = []) => {
+const injectImpls = (ctx: Provider<any> | Controller<any>, providers: Provider<unknown>[] = []) => {
   const deps: Provider<any>[] = []
   const { injectors: i } = ctx.metadata
 
   for (const item of i) {
-    if (item.implements.length > 1 && !ctx.dependencies.length)
-      throw new Error('should declare dependencies if you have more than one implements')
+    let impl: Provider<any>
 
-    const [impl] = item.implements
-    deps.push(impl)
+    if (!item.implements.length)
+      throw new Error('should have at least one implement for this interface')
+
+    if (item.implements.length > 1) {
+      const scoped = providers.find(({ metadata: { interface: { token } } }) => token === item.token)
+
+      if (!scoped && !ctx.dependencies.length)
+        throw new Error('should declare dependencies if you have more than one implements')
+
+      if (scoped)
+        impl = scoped
+    }
+
+    impl = item.implements[0]
+
+    if (impl)
+      deps.push(impl)
   }
   return deps
 }
@@ -20,22 +34,25 @@ export interface ModuleOptions<T> {
   controller: Controller<T>
   providers?: Provider<unknown>[]
   exports?: Provider<unknown>[]
+  imports?: Module<unknown>[]
 }
 
 export class Module<T> {
   public controller: Controller<T>
   public providers: Provider<unknown>[]
   public exports: Provider<unknown>[]
+  public imports: Module<unknown>[]
 
   constructor(options: ModuleOptions<any>) {
     this.controller = options.controller
     this.providers = options.providers || []
+    this.imports = options.imports || []
     this.exports = options.exports || this.providers || []
 
     this.controller.dependencies(...injectImpls(this.controller), this.providers)
 
     this.providers.forEach((provider) => {
-      provider.dependencies(...injectImpls(provider))
+      provider.dependencies(...injectImpls(provider, this.providers))
     })
   }
 
