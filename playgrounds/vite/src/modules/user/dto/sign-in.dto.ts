63f59d27211284c1dad90f072f2bc5a445bfa036
zekunjin <zekun.jin@foxmail.com>
2023-02-01 09:57:01 +0800

    chore: rm playgrounds

--- playgrounds/vite/src/modules/user/dto/sign-in.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface SignInDto {
-  username: string
-  password: string
-}
--- playgrounds/vite/src/modules/user/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { defineProvider } from '@yorjs/core'
-import { IUserRepository, IUserService } from './user.interface'
-
-export const userService = defineProvider().implements(IUserService).inject(IUserRepository).setup(repo => ({
-  async signIn(username, password) {
-    const { data } = await repo.signIn({ username, password })
-    return data
-  }
-}))
