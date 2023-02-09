0318fbc95f5db7d87179b7239857533fb750cb1e
zekunjin <zekun.jin@foxmail.com>
2023-02-09 10:25:04 +0800

    chore: rm utils

--- packages/core/src/defineValidator/index.ts
+++ packages/core/src/defineValidator/index.ts
@@ -1,8 +1,79 @@
 
-import { mapValues } from '@yorjs/shared'
-import { FieldValidate, isField, validateFeilds } from './_utils'
+import { isNumber, isObject, isString, mapValues, mapValuesDeep } from '@yorjs/shared'
 
 type FieldValues<T> = { [P in keyof T]: T[P] extends Field ? T[P]['value'] : any }
+
+export enum FieldValidate {
+  IS_STRING = 'isString',
+  IS_NUMBER = 'isNumber',
+  MIN = 'min',
+  MAX = 'max',
+  MAX_LENGTH = 'maxLength',
+  IS_ARRAY = 'isArray'
+}
+
+export const isField = (value: any): value is Field => value.constructor === Field
+export const isValidator = (value: any): value is Validator<any> => value.constructor === Validator
+
+export const validatorPolicy: Record<FieldValidate, (params: Field) => boolean> = {
+  isString: (field: Field) => isString(field.value),
+  isNumber: (field: Field) => isNumber(field.value),
+  isArray: (field: Field) => !!field,
+  min: (field: Field) => !!(field && isNumber(field.value) && field.value > Number(field._min)),
+  max: (field: Field) => !!(field && isNumber(field.value) && field.value < Number(field._max)),
+  maxLength: (field: Field) => !!field
+}
+
+export const validateFeilds = <T extends Record<string, any>>(fields: T): Promise<void> => {
+  let valid = true
+  let hasValidator = false
+
+  return new Promise((resolve, reject) => {
+    for (const [key, field] of Object.entries(fields)) {
+      if (isField(field)) {
+        for (const step of field.chains) {
+          if (!validatorPolicy[step](fields[key])) {
+            valid = false
+            break
+          }
+        }
+
+        if (!valid) { break }
+      }
+
+      if (isValidator(field)) {
+        hasValidator = true
+        field.validate().then(resolve).catch(() => {
+          valid = false
+          reject(valid)
+        })
+      }
+    }
+
+    if (!hasValidator) { valid ? resolve() : reject(valid) }
+  })
+}
+
+export const validateFeildsDeep = <T extends Record<string, any>>(fields: T): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    let valid = true
+    mapValuesDeep(fields, (field?: Field) => {
+      if (!field || !isField(field)) { return }
+
+      for (const step of field.chains) {
+        if (!validatorPolicy[step](field)) {
+          valid = false
+          reject(field)
+          break
+        }
+      }
+
+      return field
+    }, value => !isField(value) && isObject(value))
+
+    if (valid) { resolve() }
+  })
+}
 
 export class Field {
   public value: any
@@ -49,12 +120,11 @@ export class Validator<T extends Record<string, any>> {
   }
 
   get value () {
-    // const fields: any = mapValuesDeep(this._fields, ({ value }) => value, (value) => {
-    //   return !isValidator(value) && !isField(value) && isObject(value)
-    // })
-
     const fields: Record<string, any> = mapValues(this._fields, (field: any) => {
-      return isField(field) ? field.value : field
+      if (isField(field)) { return field.value }
+      if (isValidator(field)) { return field }
+
+      return field
     })
 
     return new Proxy(fields as FieldValues<T>, {
