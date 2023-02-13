b64c29995a2066e1d21186c879e4ab334a807fd5
zekunjin <zekun.jin@foxmail.com>
2023-02-13 13:07:05 +0800

    fix: rename var name

--- packages/core/__tests__/provider.spec.ts
+++ packages/core/__tests__/provider.spec.ts
@@ -1,9 +1,9 @@
 import { defineProvider } from '../src/defineProvider'
-import { controller, providerA } from './core.spec'
+import { controllerA, providerA } from './core.spec'
 
 describe('define provider', () => {
   it('should have provider dependencies in metadata', () => {
-    expect(!!controller.dependencies).toBe(true)
+    expect(!!controllerA.dependencies).toBe(true)
   })
 
   it('should have provider name as enum', () => {
