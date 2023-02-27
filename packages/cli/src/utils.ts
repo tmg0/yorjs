0781f96256a77c20cd146d32f1aa03fd2d94aea7
zekunjin <zekun.jin@foxmail.com>
2023-02-27 10:01:07 +0800

    chore: use scule

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,42 +1,19 @@
 import { existsSync, readFileSync, writeFileSync } from 'node:fs'
 import { ensureFile, mkdir, writeFile } from 'fs-extra'
 import { findStaticImports } from 'mlly'
+import { camelCase, kebabCase, pascalCase } from 'scule'
 import { Yor } from './enums'
 
-const toCamelCase = (str: string) => {
-  str = str.replace(str[0], str[0].toLowerCase())
-
-  return str.replace(/-(\w)/g, (_, item) => {
-    return item.toUpperCase()
-  })
-}
-
-const toKebabCase = (str: string) => {
-  str = str.replace(/[A-Z]/g, (item) => {
-    return `-${item.toLowerCase()}`
-  })
-
-  if (str.startsWith('-')) { return str.substr(1) }
-
-  return str
-}
-
-const toPascalCase = (str: string) => {
-  str = toCamelCase(str)
-
-  return str.replace(str[0], str[0].toUpperCase())
-}
-
-const I = (str: string, suffix = 'provider') => `I${toPascalCase(str)}${toPascalCase(suffix)}`
+const I = (str: string, suffix = 'provider') => `I${pascalCase(str)}${pascalCase(suffix)}`
 
 const interfaceImportStringify = (lib: string, str: string, suffix: string[] = []) => {
   const moduleArr = suffix.map(el => I(str, el))
   return `import { ${moduleArr.join(', ')} } from '${lib}'`
 }
 
-const controllerTemplate = (str: string, dependencies: string[] = []) => {
+export const controllerTemplate = (str: string, dependencies: string[] = []) => {
   return `import { defineController } from '@yorjs/core'
-${interfaceImportStringify(`./${toKebabCase(str)}.interface`, str, ['controller', ...dependencies])}
+${interfaceImportStringify(`./${kebabCase(str)}.interface`, str, ['controller', ...dependencies])}
 
 export const ${str}Controller = defineController().implements(${I(str, 'controller')}).inject(${dependencies.map(el => I(str, el)).join(', ')}).setup((${dependencies.join(', ')}) => {
   return {
@@ -46,15 +23,15 @@ export const ${str}Controller = defineController().implements(${I(str, 'controll
 `
 }
 
-const interfaceTemplate = (str: string, suffix = 'provider') => {
-  return `export const ${I(toPascalCase(str), suffix)} = defineInterface<{/** */}>()\n`
+export const interfaceTemplate = (str: string, suffix = 'provider') => {
+  return `export const ${I(pascalCase(str), suffix)} = defineInterface<{/** */}>()\n`
 }
 
-const providerTemplate = (str: string, dependencies: string[] = [], suffix = 'provider') => {
+export const providerTemplate = (str: string, dependencies: string[] = [], suffix = 'provider') => {
   return `import { defineProvider } from '@yorjs/core'
-${interfaceImportStringify(`./${toKebabCase(str)}.interface`, str, [suffix, ...dependencies])}
+${interfaceImportStringify(`./${kebabCase(str)}.interface`, str, [suffix, ...dependencies])}
 
-export const ${str}${toPascalCase(suffix)} = defineProvider().implements(${I(str, suffix)}).inject(${dependencies.map(el => I(str, el)).join(', ')}).setup((${dependencies.join(', ')}) => {
+export const ${str}${pascalCase(suffix)} = defineProvider().implements(${I(str, suffix)}).inject(${dependencies.map(el => I(str, el)).join(', ')}).setup((${dependencies.join(', ')}) => {
   return {
     /** */
   }
@@ -64,19 +41,19 @@ export const ${str}${toPascalCase(suffix)} = defineProvider().implements(${I(str
 
 const moduleTemplate = (str: string) => {
   return `import { defineModule } from '@yorjs/core'
-import { ${toCamelCase(str)}Controller } from './${toKebabCase(str)}.controller'
-import { ${toCamelCase(str)}Service } from './${toKebabCase(str)}.service'
-import { ${toCamelCase(str)}Repo } from './${toKebabCase(str)}.repo'
+import { ${camelCase(str)}Controller } from './${kebabCase(str)}.controller'
+import { ${camelCase(str)}Service } from './${kebabCase(str)}.service'
+import { ${camelCase(str)}Repo } from './${kebabCase(str)}.repo'
 
-export const ${toCamelCase(str)}Module = defineModule({
-  controller: ${toCamelCase(str)}Controller,
-  providers: [${toCamelCase(str)}Service, ${toCamelCase(str)}Repo]
+export const ${camelCase(str)}Module = defineModule({
+  controller: ${camelCase(str)}Controller,
+  providers: [${camelCase(str)}Service, ${camelCase(str)}Repo]
 })
 `
 }
 
 const genInterface = async (str: string, path = '.', suffix = 'provider') => {
-  const YOR_INTERFACE_FILE_NAME = `${path}/${toKebabCase(str)}.interface.ts`
+  const YOR_INTERFACE_FILE_NAME = `${path}/${kebabCase(str)}.interface.ts`
   await ensureFile(YOR_INTERFACE_FILE_NAME)
 
   let fileStr = readFileSync(YOR_INTERFACE_FILE_NAME).toString()
@@ -88,22 +65,22 @@ const genInterface = async (str: string, path = '.', suffix = 'provider') => {
 }
 
 const genProvider = async (str: string, path = '.', dependencies: string[] = [], suffix = 'provider') => {
-  const YOR_PROVIDER_FILE_NAME = `${path}/${toKebabCase(str)}.${suffix}.ts`
+  const YOR_PROVIDER_FILE_NAME = `${path}/${kebabCase(str)}.${suffix}.ts`
   await ensureFile(YOR_PROVIDER_FILE_NAME)
 
   writeFile(YOR_PROVIDER_FILE_NAME, providerTemplate(str, dependencies, suffix))
 }
 
 const genController = async (str: string, path = '.', dependencies: string[] = []) => {
-  const YOR_INTERFACE_FILE_NAME = `${path}/${toKebabCase(str)}.controller.ts`
+  const YOR_INTERFACE_FILE_NAME = `${path}/${kebabCase(str)}.controller.ts`
   await ensureFile(YOR_INTERFACE_FILE_NAME)
 
   writeFile(YOR_INTERFACE_FILE_NAME, controllerTemplate(str, dependencies))
 }
 
 const genModule = async (str: string, path = '.') => {
-  const YOR_MODULR_PATH = `${path}/${toKebabCase(str)}`
-  const YOR_MODULE_FILE_NAME = `${YOR_MODULR_PATH}/${toKebabCase(str)}.module.ts`
+  const YOR_MODULR_PATH = `${path}/${kebabCase(str)}`
+  const YOR_MODULE_FILE_NAME = `${YOR_MODULR_PATH}/${kebabCase(str)}.module.ts`
 
   if (!existsSync(YOR_MODULR_PATH)) { mkdir(YOR_MODULR_PATH) }
 
@@ -125,10 +102,4 @@ export const templates = {
   [Yor.INTERFACE]: genInterface,
   [Yor.PROVIDER]: genProvider,
   [Yor.CONTROLLER]: genController
-}
-
-export {
-  controllerTemplate,
-  interfaceTemplate,
-  providerTemplate
 }
